a549abba6c2ae3e80718575a55b01c00e1fc6b0e
Alex Skreen <skreenalex@gmail.com>
2020-02-07 16:52:48 -0800

    add second test, got meaningful fail

--- src/sgac2.js
+++ src/sgac2.js
@@ -6,7 +6,7 @@ export class User {
 
   earthAge() {
     if (this.age > 0) {
-      let userEarthAge = (this.age * 1);
+      let userEarthAge = (this.age * 467);
       return userEarthAge;
     } else if (this.age <= 0) {
       return "error. Please submit real age";
