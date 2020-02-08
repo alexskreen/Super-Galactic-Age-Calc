176c37ac9bfb552dc1d01719871a63aaeecf987e
Alex Skreen <skreenalex@gmail.com>
2020-02-07 16:54:19 -0800

    added method to calculate earth age. passed test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,16 +1,14 @@
-import { User } from './../src/sgac2.js';
+import { User } from "./../src/sgac2.js";
 import { exportAllDeclaration } from "@babel/types";
 
-describe('User', () => {
-  test('should create new user from user class', () => {
+describe("User", () => {
+  test("should create new user from user class", () => {
     const user = new User("alex", 30);
     expect(user.age).toEqual(30);
   });
 
-  test('should calculate user age on earth', () => {
+  test("should calculate user age on earth", () => {
     let user = new User("alex", 30);
-    expect(user.earthAge()).toEqual("blah");
-    console.log(this.earthAge)
-    console.log(earthAge);
-  })
+    expect(user.earthAge()).toEqual(user.age);
+  });
 });
--- src/sgac2.js
+++ src/sgac2.js
@@ -6,7 +6,7 @@ export class User {
 
   earthAge() {
     if (this.age > 0) {
-      let userEarthAge = (this.age * 467);
+      let userEarthAge = (this.age * 1);
       return userEarthAge;
     } else if (this.age <= 0) {
       return "error. Please submit real age";
